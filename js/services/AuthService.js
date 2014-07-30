e82729a0ffa00ae1cd89a877ddaf4d7393ed9eda
Alastair Paragas <alastairparagas@gmail.com>
2014-07-30 02:08:49 -0400

    Auth: Added settings feature and pull of user id on login
    Added settings feature that is stored on localStorage and pulled in for
    that particular user using our application. We also pull their user id
    from the server on login and use that id as one of the ways we can
    identify the user so that when we are traversing over the gallery images,
    if a user has rated a certain image or uploaded it him/herself, he/she
    cannot rate it.

--- js/services/AuthService.js
+++ js/services/AuthService.js
@@ -4,27 +4,52 @@
 // an object.
 
 angular.module('rateMeApp.services').service('AuthService', function(Base64, $q, $http, $cordovaNetwork, $cordovaToast){
+    "use strict";
+    
     // Use an object to prevent too much global variables - very bad.
     // See Douglas Crockford's "Javascript: The Good Parts"
     this.isLoggedIn = false;
+    this.userid = 0;
     this.username = null;
+    this.password = null;
+    var settings = {}
+    var settingsDefaults = {
+        smartUpload: true
+    };
     
     // Helper methods for our Authentication service
     // Set the Authentication properties
-    this.setUser = function(username, password){
+    this.setUser = function(username, password, userid){
         this.encodedCredentials = Base64.encode(username + ":" + password);
         // Set Angular Authorization headers for Stateless HTTP Basic
         $http.defaults.headers.common['Authorization'] = 'Basic ' + this.encodedCredentials;
+        this.userid = userid;
         this.username = username;
         this.password = password;
         this.isLoggedIn = true;
+        settings = (localStorage.settings && JSON.parse(localStorage.settings)) || {};
+        for(var settingsKey in settingsDefaults){
+            settings[settingsKey] = settings[settingsKey] || settingsDefaults[settingsKey];
+        }
     }
     
     // Create "permanent" storage of user information
-    this.permStorage = function(username, password){
+    this.permStorage = function(username, password, userid){
+        localStorage.userid = userid;
         localStorage.username = username;
         localStorage.password = password;
         localStorage.encodedCredentials = Base64.encode(username + ":" + password);
+    }
+    
+    // Set user settings
+    this.setSetting = function(key, value){
+        settings[key] = value;
+        localStorage.settings = JSON.stringify(settings);
+    }
+    
+    // Get user settings. It doesn't exist? Return null
+    this.getSetting = function(key){
+        return settings[key] || null;
     }
     
     // Attempts to log the user in with stateless HTTP Basic Auth that we set server-side with Laravel
@@ -34,26 +59,27 @@ angular.module('rateMeApp.services').service('AuthService', function(Base64, $q,
         // data, at which point Javascript already executes the rest of the code, interpreting the function as it was before.
         var defer = $q.defer();
         var messageBag = {};
-        var aUsername, aPassword;
+        var aUsername, aPassword, aUserid;
         // We need to set a variable that sets the correct context of "this" to this function
         // because when we use internal callback functions within this function, "this" gets
         // set to that function's scope and not this parent/outside function's scope
         var that = this;
         
         // If already logged in, resolve the promise!
-        if(this.isLoggedIn === true && this.username != "undefined" && this.password != "undefined"){
-            defer.resolve({message: "Already logged in"});
+        if(this.isLoggedIn === true){
+            defer.resolve({message: "Already logged in!"});
             return defer.promise;
         }
         
         // Variable to hold the decision. If no username/password was provided, that is a command to check localStorage instead
         // of passed inputs. Two possible values: storage (to check localStorage) and input (to check passed parameters/inputs).
-        // In this case, we shall use == instead of === because we need valid passwords, not undefined and null.
+        // In this case, we shall use == instead of === because we are targeting for both undefined and nulls.
         var inputOrStorage;
         if(typeof username == "undefined" && typeof password == "undefined"){
             inputOrStorage = "storage";
             aUsername = localStorage.username;
             aPassword = localStorage.password;
+            aUserid = localStorage.userid;
         }else{
             inputOrStorage = "input";
             aUsername = username;
@@ -69,17 +95,23 @@ angular.module('rateMeApp.services').service('AuthService', function(Base64, $q,
         // If storage, only set the Object properties using the localStorage stored information, else
         // "input" option - use passed parameters
         if(inputOrStorage === "storage"){
-            that.setUser(aUsername, aPassword);
+            this.setUser(aUsername, aPassword, aUserid);
             messageBag.message = "Automatically logging in.";
             defer.resolve(messageBag);
         }else{
+            if($cordovaNetwork.isOffline()){
+                $cordovaToast.show("No internet connection.", "long", "bottom");
+                defer.reject({message: "No internet connection. Cannot check credentials."});
+                return defer.promise;    
+            }
+            
             $http.post("http://myrighttoplay.com/R8M3/public/api/v1/user/login", {'username': aUsername, 'password': aPassword})
-            .success(function(data){
-                that.setUser(aUsername, aPassword);
-                that.permStorage(aUsername, aPassword);
-                defer.resolve(data);
-            }).error(function(data){
-                defer.reject(data);
+            .success(function(response){
+                that.setUser(aUsername, aPassword, response.info.id);
+                that.permStorage(aUsername, aPassword, response.info.id);
+                defer.resolve(response);
+            }).error(function(response){
+                defer.reject(response);
             });
         }
 
@@ -100,6 +132,7 @@ angular.module('rateMeApp.services').service('AuthService', function(Base64, $q,
         // Delete Basic Authorization set header
         delete $http.defaults.headers.common.Authorization;
         // Delete the username/password properties, set isLoggedIn to false
+        delete this.userid;
         delete this.username;
         delete this.password;
         delete this.encodedCredentials;
@@ -116,11 +149,17 @@ angular.module('rateMeApp.services').service('AuthService', function(Base64, $q,
         var defer = $q.defer();
         var that = this;
         
+        if($cordovaNetwork.isOffline()){
+            $cordovaToast.show("No internet connection.", "long", "bottom");
+            defer.reject({message: "No internet connection. Cannot add in server."});
+            return defer.promise;
+        }
+        
         $http.post("http://myrighttoplay.com/R8M3/public/api/v1/user/add", infoData)
         .success( function(data){
             // Log them in
-            that.setUser(infoData.username, infoData.password);
-            that.permStorage(infoData.username, infoData.password);
+            that.setUser(infoData.username, infoData.password, data.info.id);
+            that.permStorage(infoData.username, infoData.password, data.info.id);
             defer.resolve(data);
         })
         .error( function(data){

156e47bb586e1d9081277dfc6349ee11329f197b
Alastair Paragas <alastairparagas@gmail.com>
2014-07-30 02:00:12 -0400

    AppCtrl: $state vs $location, bug-fix on about modal
    Utilizing $state instead of $location service for cleaner implementations.
    Also fixed the bug on modal where if we click on the license, it does not
    load the respective url on the system browser, which was fixed by adding a
    scope to the $ionicPopup service. Also removed the $scope.isOnlineNotify
    from our AppCtrl, opting to check for internet connections in our services
    so that Controllers have to worry less about logic and more about just
    transmitting data from view and our services to each other.

--- js/controllers/AppCtrl.js
+++ js/controllers/AppCtrl.js
@@ -1,6 +1,6 @@
 var module = angular.module('rateMeApp.controllers');
 
-module.controller('AppCtrl', function(AuthService, $ionicModal, $ionicSideMenuDelegate, $ionicPopup, $scope, $location, $cordovaNetwork, $cordovaToast) {
+module.controller('AppCtrl', function(AuthService, $ionicModal, $ionicSideMenuDelegate, $ionicPopup, $scope, $state, $cordovaNetwork, $cordovaToast) {
 
     $scope.register = {};
     $scope.login = {};
@@ -19,17 +19,11 @@ module.controller('AppCtrl', function(AuthService, $ionicModal, $ionicSideMenuDe
         $scope.register.modal = modal;
     });
     
+    
     // Checks if user is logged in.
     $scope.isLoggedIn = function(){
         $scope.user.username = AuthService.username;
         return AuthService.isLoggedIn;
-    }
-    
-    // Notifies whether the user is online or not.
-    $scope.isOnlineNotify = function(){
-        if($cordovaNetwork.isOffline()){
-            $cordovaToast.show("You currently have no internet connection.", "long", "bottom");
-        }
     }
     
     // Checks if device is online.
@@ -39,9 +33,8 @@ module.controller('AppCtrl', function(AuthService, $ionicModal, $ionicSideMenuDe
     
     
     // Tries to log the person in
-    $scope.login.submit = function() { 
+    $scope.login.submit = function() {
         
-        $scope.isOnlineNotify();
         delete $scope.login.message;
         
         AuthService.login($scope.login.data.username, $scope.login.data.password)
@@ -49,7 +42,7 @@ module.controller('AppCtrl', function(AuthService, $ionicModal, $ionicSideMenuDe
             function (data){
                 $scope.login.modal.hide();
                 delete $scope.login.data;
-                $location.path('/app/gallery');
+                $state.go('app.gallery');
             },
             function (data){
                 $scope.login.message = data.message;
@@ -57,18 +50,16 @@ module.controller('AppCtrl', function(AuthService, $ionicModal, $ionicSideMenuDe
         );
     };
     
-    
     // Logs the user out and redirects back to home page
     $scope.login.logout = function(){
         AuthService.logout();
-        $location.path('/app/home');
+        $state.go('app.home');
     };
     
     
     // Registers the user and automatically logs them in
     $scope.register.submit = function() {
         
-        $scope.isOnlineNotify();
         delete $scope.register.message;
         
          AuthService.register($scope.register.data)
@@ -83,16 +74,21 @@ module.controller('AppCtrl', function(AuthService, $ionicModal, $ionicSideMenuDe
          );
     };
     
+    
     $scope.about = function() {
         $ionicPopup.alert({
             title: 'About R8M3',
-            template: 'R8M3 is an anonymous photo-sharing and rating application designed by Alastair Paragas and Anil Jason of Stela Inc. Copyright 2014. <br/><br/> If you have any suggestions or recommendations on how we can best improve the app, do not hesitate to contact us at us@r8m3.com. <br/><br/> Interested in Stela? We are a group of active programmers and designers bent on creating products that promote freedom and transparency.'
+            templateUrl: "templates/about.html",
+            scope: $scope
         });
     };
     
     
-    $scope.disableSideMenu = function() {
-        $ionicSideMenuDelegate.canDragContent(false);  
+    $scope.externalLink = function(url) {
+        if(url != "undefined"){
+            window.open(encodeURI(url), "_system"); 
+        }
+        return false;
     };
     
 });

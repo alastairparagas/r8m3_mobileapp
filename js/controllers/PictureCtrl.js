82c6e521959a541b0a5605fc2de914ae196255a7
Alastair Paragas <alastairparagas@gmail.com>
2014-07-30 02:08:36 -0400

    Moved Gallery logic GalleryCtrl/Services

--- js/controllers/PictureCtrl.js
+++ js/controllers/PictureCtrl.js
@@ -1,9 +1,8 @@
 var module = angular.module('rateMeApp.controllers');
 
-module.controller('PictureCtrl', function(PictureService, $scope, $location, $cordovaDialogs, $cordovaToast){
+module.controller('PictureCtrl', function(PictureService, $scope, $location, $cordovaToast){
     
     $scope.picture = {};
-    
     
     // Opens the built-in camera application and grabs the taken image
     $scope.picture.takePicture = function() {
@@ -30,14 +29,10 @@ module.controller('PictureCtrl', function(PictureService, $scope, $location, $co
     // Uploads the picture to our servers
     $scope.picture.uploadPicture = function(fileUrl){
         
+        // Reset interface
         delete $scope.picture.data;
         $scope.picture.failed = false;
-        
-        // Pre-flight checks
-        $scope.isOnlineNotify();
-        if( typeof fileUrl == "undefined" ){
-            $location.path('/app/home');
-        }
+        $scope.picture.progress = 0;
         
         // Upload with Qs
         PictureService.uploadPicture(fileUrl)
@@ -47,12 +42,18 @@ module.controller('PictureCtrl', function(PictureService, $scope, $location, $co
                 $scope.picture.data = data;
             },
             function (data){
-                $scope.picture.status = true;
+                $scope.picture.failed = true;
                 $scope.picture.progress = 100;
                 $scope.picture.data = data;
             },
             function (data){
-                $scope.picture.progress = data;
+                if(data < 35){
+                    $scope.picture.progress = 35;
+                    return;
+                }
+                if(data > $scope.picture.progress){
+                    $scope.picture.progress = data;
+                }
             }
         );
         
--- js/services/PictureService.js
+++ js/services/PictureService.js
@@ -1,18 +1,11 @@
-angular.module('rateMeApp.services').service('PictureService', function(AuthService, $q, $cordovaNetwork, $cordovaToast, $cordovaFile, $cordovaCamera){
+angular.module('rateMeApp.services').service('PictureService', function(AuthService, $q, $http, $state, $cordovaNetwork, $cordovaToast, $cordovaFile, $cordovaCamera) {
     
-    this.getLimit = 30;
-    this.picCounter = 0;
-    this.pictureList = [];
-    
-    
-    // Provides the Camera interface and the whole picture upload/page redirection magic.
-    this.takePicture = function(){
-        // Asynchrous preparation
+    // Provides the camera interface and takes picture
+    this.takePicture = function () {
         var defer = $q.defer();
         
         // Cordova Camera Options https://github.com/apache/cordova-plugin-camera/blob/master/doc/index.md
-        var options = { 
-            quality : 100, 
+        var options = {  
             destinationType : Camera.DestinationType.FILE_URI, 
             correctOrientation: true,
             sourceType : Camera.PictureSourceType.CAMERA, 
@@ -20,6 +13,17 @@ angular.module('rateMeApp.services').service('PictureService', function(AuthServ
             encodingType: Camera.EncodingType.PNG,
             saveToPhotoAlbum: false
         };
+        // Smart-Upload feature
+        if(AuthService.getSetting("smartUpload")){
+            if(navigator.connection.type == Connection.ETHERNET || navigator.connection.type == Connection.Wifi){
+                options.quality = 100;
+            }else{
+                options.quality = 60;   
+            }
+        }else{
+            options.quality = 60;
+        }
+        
         // Take a picture, redirect to page with passed image location
         $cordovaCamera.getPicture(options).then(function(imageData) {
             defer.resolve(imageData);
@@ -39,7 +43,22 @@ angular.module('rateMeApp.services').service('PictureService', function(AuthServ
         // Pictures can only be taken after 10 minute increments
         if( localStorage.lastSnap && new Date().getTime() - localStorage.lastSnap < 600000 ){
             $cordovaToast.show("Pictures can only be taken in 10 minute increments.", "long", "bottom");
-            defer.reject({message: "Pictures can only be taken after 10 minute increments."});
+            defer.reject({message: "Pictures can only be taken in 10 minute increments."});
+            return defer.promise;
+        }
+        
+        // No internet connection, stop upload
+        if($cordovaNetwork.isOffline()){
+            $cordovaToast.show("No internet connection. Cannot upload.", "long", "bottom");
+            defer.reject({message: "No internet connection. Cannot upload"});
+            return defer.promise;
+        }
+        
+        // filePath isn't valid, stop upload
+        if( typeof filePath == "undefined" ){
+            $cordovaToast.show("The file does not exist/invalid file path", "long", "bottom");
+            $state.go('app.home');
+            defer.reject({message: "File/Path doesn't exist"});
             return defer.promise;
         }
         
@@ -71,20 +90,6 @@ angular.module('rateMeApp.services').service('PictureService', function(AuthServ
         );
         
         return defer.promise;
-    };
-    
-    
-    // Gets random pictures from server for user to rate.
-    // First parameter - order by: id, popularity, random
-    this.getPictures = function(){
-        
-    };
-    
-    
-    // Gets a picture's information given an imageId.
-    // Returns an object holding information about the image.
-    this.getPicture = function(imageId){
-          
     };
     
 });

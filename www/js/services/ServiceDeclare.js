08dec0b2682a9562de1bee04ce8f98b73467c3fe
Alastair Paragas <alastairparagas@gmail.com>
2014-08-18 19:42:12 -0400

    Updated Ionic package and moved application logic

--- www/js/services/ServiceDeclare.js
+++
@@ -1,3 +0,0 @@
-// You declare a module with its name and the array of dependencies.
-// You then refer to the already initialized module by calling only its name without a dependency parameter.
-angular.module('rateMeApp.services', []);
